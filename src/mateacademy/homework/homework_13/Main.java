c5dd0d42d990075d9e0ef4a9a82441c5b06d63fd
UKiyanitsa <u.kiyanitsa@gmail.com>
2019-06-24 18:58:47 +0300

    Initial commit

---
+++ src/mateacademy/homework/homework_13/Main.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+}
